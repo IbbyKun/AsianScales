ad485bf8b3c5652c65066fe40cf24ad3ba5e1dbc
Ibrahim <muh.ibrahim240102@gmail.com>
2025-01-01 14:58:27 +0500

    fix: 3D models remdering

--- src/components/Model.js
+++ src/components/Model.js
@@ -1,5 +1,6 @@
 import { useGLTF } from '@react-three/drei';
-import { memo } from 'react';
+import { memo, useEffect } from 'react';
+import * as THREE from 'three';
 
 // Preload the models with absolute paths
 useGLTF.preload('/models/model1.glb');
@@ -8,7 +9,21 @@ useGLTF.preload('/models/model3.glb');
 
 const Model = ({ modelPath }) => {
   const { scene } = useGLTF(modelPath);
-  return <primitive object={scene} scale={1.5} position={[0, -1, 0]} />;
+  
+  useEffect(() => {
+    // Center the model using bounding box
+    const box = new THREE.Box3().setFromObject(scene);
+    const center = box.getCenter(new THREE.Vector3());
+    scene.position.sub(center); // Center the model
+  }, [scene]);
+
+  return (
+    <primitive 
+      object={scene} 
+      scale={1.5} 
+      position={[0, 0, 0]}
+    />
+  );
 };
 
 export default memo(Model); 
